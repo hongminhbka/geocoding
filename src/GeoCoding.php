aa66ff93d0cacc00bcb702e87ffe19e0001c2cd7
tongdv <hongminhbka@gmail.con>
2018-01-19 23:34:02 +0700

    init code

---
+++ src/GeoCoding.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: tong.dv
+ * Date: 19/01/2018
+ * Time: 23:16
+ */
+class GeoCoding
+{
+    public function getGeoCoding()
+    {
+        return 1;
+    }
+}
